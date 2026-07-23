9c0eb5d19b36f4e753d940e57d534936982d8727
marocoder-creates <janek.janek.6797@gmail.com>
2026-07-23 12:22:28 +0200

    undo redo in rooms and designs change of snackbar geometry helper fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1066525167" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1941990634" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
